1739e10321f4a8b150b5a550a5f09cca3c21622e
Tung <96duongtung@gmail.com>
2024-09-04 15:13:47 +0700

    add image happyhouse and add wedding project

--- app/porfolio/page.tsx
+++ app/porfolio/page.tsx
@@ -17,27 +17,27 @@ const items = [
   {
     id: 2,
     color: "from-blue-300 to-violet-300",
-    title: "Catfish Management System",
-    desc: "Developed a digital data management system for Pangasius fish farms, addressing the inefficiencies of manual processes using paper and Excel. Functions include: Technicians proposing, Regional Directors approving, Warehouse Managers confirming, Adjusting quantities due to weather, Proposing medication, Reporting dead fish, Inventory management, and Harvesting",
-    img: "/images/BAOMI.png",
-    link:"https://baomi.vn"
+    title: "Online wedding invitations",
+    desc: "A simple website for online wedding invitations",
+    img: "/images/wedding.png",
+    link:"https://tungthamwedding.tungdev1996.online/?name=Ch%E1%BB%8B%20Th%E1%BA%AFm%20v%C3%A0%20gia%20%C4%91%C3%ACnh"
   },
   {
     id: 3,
     color: "from-violet-300 to-purple-300",
     title: "Ayumi School Project(Happy House kindergarten)",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Develop an app for managing a kindergarten, featuring student and parent check-in/check-out with face detection, notifications, messaging and chat, student reviews, and compatibility with website, iOS, and Android platforms.",
+    img: "/images/happyHouse.png",
     link: "porfolio/Ayumi-School-Project",
   },
-  // {
-  //   id: 4,
-  //   color: "from-purple-300 to-red-300",
-  //   title: "Spotify Music App",
-  //   desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-  //   img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  //   link: "https://lama.dev",
-  // },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Catfish Management System",
+    desc: "Developed a digital data management system for Pangasius fish farms, addressing the inefficiencies of manual processes using paper and Excel. Functions include: Technicians proposing, Regional Directors approving, Warehouse Managers confirming, Adjusting quantities due to weather, Proposing medication, Reporting dead fish, Inventory management, and Harvesting",
+    img: "/images/BAOMI.png",
+    link:"https://baomi.vn"
+  },
 ];
 
 const PortfolioPage = () => {
@@ -101,7 +101,7 @@ const PortfolioPage = () => {
             </defs>
             <text fill="#000">
               <textPath xlinkHref="#circlePath" className="text-xl text-white">
-                Fresher Back-end Developer
+                Back-end Developer
               </textPath>
             </text>
           </motion.svg>
